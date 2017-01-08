5630c34efb347a6b117db4277b8770dcb6185c77
Damien Zammit <damien@zamaudio.com>
2017-01-08 17:32:14 +1100

    src/cpu: Point microcode files to new submodule location
    Signed-off-by: Damien Zammit <damien@zamaudio.com>

--- src/cpu/amd/model_fxx/microcode_blob.c
+++ src/cpu/amd/model_fxx/microcode_blob.c
@@ -13,6 +13,6 @@
 
 unsigned char microcode[] __attribute__ ((aligned(16))) = {
 #if !CONFIG_K8_REV_F_SUPPORT
-#include "../../../../3rdparty/blobs/cpu/amd/model_fxx/microcode.h"
+#include "../../../../3rdparty/microcode/amd/model_fxx/microcode.h"
 #endif
 };
